fcfc0fe48f2ba8c2de178caf668e9e92e34e7164
Ian Wilcox and vichitra pool <ian+vichitra@>
2016-08-11 10:13:00 -0700

    completed js page

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
   var over18 = confirm("Are you over 18; Click ok for YES, cancel for NO");
-    if over18 {
-      $(.)
-    }
+    if (over18) {
+      $(".olderthan18").show();
+    } else {
+      $(".notquite18").show();
+  }
 
   var candidates = $("input:radio[name=candidates]:checked").val();
 
